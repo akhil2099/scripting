ae7cc71163b42d948c0e212004044187d72a993a
akhil2099 <akhilvimal7@gmail.com>
2023-07-27 20:55:04 +0530

    new changes

--- ShellScript.sh
+++ ShellScript.sh
@@ -7,15 +7,28 @@ error="\033[1;31m"
 nocolour="\033[00m"
 
 if [[ -d /home/akhil/Downloads/Web ]];then
-echo -e $alert directory exists $nocolour
+echo -e $alert Directory exists $nocolour
 else
 mkdir -p /home/akhil/Downloads/Web
 echo -e $success Directory created $nocolour
 fi
 
-if [[ -f /home/akhil/Downloads/Web/latest.zip ]];then
-echo -e $warning file already exist $nocolour
+if [[ -f /home/akhil/Downloads/Web/bootstrap/bootstrap-4.0.0-dist.zip ]];then
+echo -e $warning File already exist $nocolour
 else
-wget https://wordpress.org/latest.zip -o /home/akhil/Downloads/Web/latest.zip
-echo -e $success file Downloaded successfully $nocolour
+wget https://github.com/twbs/bootstrap/releases/download/v4.0.0/bootstrap-4.0.0-dist.zip -o /home/akhil/Downloads/Web/bootstrap/bootstrap-4.0.0-dist.zip
+echo -e $success File Downloaded successfully $nocolour
+fi
+
+
+if [[ /home/akhil/Downloads/Web/bootstrap ]];then
+ echo -e $warning Bootstrap Directory already exist $nocolour
+else
+mkdir home/akhil/Downloads/Web/bootstrap
+fi
+
+if [[ -f /home/akhil/Downloads/Web/bootstrap/* ]]; then
+    echo -e $warning File already extracted $nocolour
+else
+    unzip -d /home/akhil/Downloads/Web/bootstrap bootstrap-4.0.0-dist.zip
 fi
