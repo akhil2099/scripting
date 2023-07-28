66c6a1ae0ca0e61a1c912a22e3c10d000df84af4
akhil2099 <akhilvimal7@gmail.com>
2023-07-28 18:09:34 +0530

    script updated

--- wordpress.sh
+++ wordpress.sh
@@ -6,35 +6,39 @@ warning="\033[1;33m"
 error="\033[1;31m"
 nocolour="\033[00m"
 
+if [[ $EUID -eq 0 ]];then
 
-if [[ -f /home/akhil/Downloads/latest.zip ]];then
-echo -e $alert File exists $nocolour
-elif [[ ! -f /home/akhil/Downloads/latest.zip ]];then
-echo -e $alert Do you want to install wordpress yes[Y], no[N] $nocolour
-read answer
-    if [[ $answer == Y ]];then
-    wget https://wordpress.org/latest.zip -o /home/akhil/Downloads/latest.zip
-    echo -e $success File Downloaded successfully $nocolour
+    if [[ -f /home/akhil/Downloads/latest.zip ]];then
+    echo -e $alert File exists $nocolour
+    elif [[ ! -f /home/akhil/Downloads/latest.zip ]];then
+    echo -e $alert Do you want to install wordpress yes[Y], no[N] $nocolour
+    read answer
+        if [[ $answer == Y ]];then
+        wget https://wordpress.org/latest.zip -o /home/akhil/Downloads/latest.zip
+        echo -e $success File Downloaded successfully $nocolour
+        else
+        echo -e $warning Installation Aborted $nocolour
+        fi
     else
-    echo -e $warning Installation Aborted $nocolour
+        exit    
     fi
+
+
+    if [[ -f /home/akhil/Downloads/latest.zip ]]; then
+        unzip -q -d /var/www/html/ latest.zip
+        echo -e $success Extraction Successful $nocolour
+    else
+    echo -e $error unzip failed $nocolour
+    fi
+
+
+    if [[ -d /var/www/html/wordpress ]];then
+    chmod -R 750 /var/www/html/wordpress
+    echo -e $success File permission granted $nocolour
+    else
+    echo -e $error File permission rejected $nocolour
+    fi
+
 else
-    exit    
+echo -e $warning run in sudo privilege $nocolour
 fi
-
-
-if [[ -f /home/akhil/Downloads/latest.zip ]]; then
-    unzip -q -d /var/www/html/ latest.zip
-    echo -e $success Extraction Successful $nocolour
-else
-   echo -e $error unzip failed $nocolour
-fi
-
-
-if [[ -d /var/www/html/wordpress ]];then
-sudo chmod -R 750 /var/www/html/wordpress
-echo -e $success File permission granted $nocolour
-else
-echo -e $error File permission rejected $nocolour
-fi
-
