51686b8d97e128ce221c20907086e9a8e277c007
akhil2099 <akhilvimal7@gmail.com>
2023-08-10 20:10:33 +0530

    server information script committed

---
+++ index1.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>AKHIL</title>
+    </head>
+    <body>
+        <h2 align="center"></h2>
+    </body>
+    </html>
+<?php
+$hostname=shell_exec("hosname");
+
+
+?>
